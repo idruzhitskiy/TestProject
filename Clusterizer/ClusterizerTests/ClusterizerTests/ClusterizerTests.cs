e77a129389389997cec2eed68c61bcbf3a14e89e
idruzhitskiy <idruzhitskiy@mail.ru>
2016-09-12 20:10:29 +0300

    Added exception tests

--- Clusterizer/ClusterizerTests/ClusterizerTests/ClusterizerTests.cs
+++ Clusterizer/ClusterizerTests/ClusterizerTests/ClusterizerTests.cs
@@ -25,12 +25,32 @@ namespace ClusterizerTests.ClusterizerTests
             var clusterizer = kernel.Get<IClusterizer>();
             var oneElement = new List<IEntity> { CreateEntity(new[] { new[] { "1" } }) };
             var twoElements = new List<IEntity> { CreateEntity(new[] { new[] { "1" } }), CreateEntity(new[] { new[] { "2" } }) };
+            bool exceptionOnZeroClusters = false;
+            bool exceptionOnEmptyList = false;
 
             // act
+            try
+            {
+                var temp = clusterizer.Clusterize(twoElements, 0);
+            }
+            catch
+            {
+                exceptionOnZeroClusters = true;
+            }
+            try
+            {
+                var temp = clusterizer.Clusterize(new List<IEntity>(), 2);
+            }
+            catch
+            {
+                exceptionOnEmptyList = true;
+            }
             var oneElementOneCluster = clusterizer.Clusterize(oneElement, 1);
             var twoClusters = clusterizer.Clusterize(twoElements, 2);
 
             // assert
+            Assert.IsTrue(exceptionOnZeroClusters);
+            Assert.IsTrue(exceptionOnEmptyList);
             Assert.IsTrue(oneElementOneCluster.Count == 1);
             Assert.IsTrue(twoClusters.Count == 2);
         }
