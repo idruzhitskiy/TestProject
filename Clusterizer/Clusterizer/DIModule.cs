a7a155133ab0f16cfa05ba5c494cc844b2bd3cdc
idruzhitskiy <idruzhitskiy@mail.ru>
2016-09-13 19:59:45 +0300

    Implemented

--- Clusterizer/Clusterizer/DIModule.cs
+++ Clusterizer/Clusterizer/DIModule.cs
@@ -1,4 +1,5 @@
 ﻿using Clusterizer.DistanceFunctions;
+using Clusterizer.Entities;
 using Ninject.Modules;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,7 @@ namespace Clusterizer
             // Сюда добавлять реализации абстрактных классов, например
             // Bind<Interface>().To<Realisation>()
             Bind<IDistanceFunction>().To<DistanceFunction>();
+            Bind<IEntitiesFactory>().To<SimpleEntitiesFactory>();
         }
     }
 }
---
+++ Clusterizer/Clusterizer/Entities/SimpleEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clusterizer.Entities
+{
+    public class SimpleEntity : IEntity
+    {
+        private readonly List<List<string>> testAttributes;
+
+        public SimpleEntity(List<List<string>> attributes)
+        {
+            this.testAttributes = attributes;
+        }
+
+        public List<List<string>> TextAttributes
+        {
+            get
+            {
+                return testAttributes;
+            }
+        }
+    }
+}
